d1ef4de1e38a0c4ee2fe59ad2eb9144983cedccc
stellaho0511 <86877131+stellaho0511@users.noreply.github.com>
2023-02-27 01:05:16 +0900

    flask deploy

---
+++ app.py
@@ -0,0 +1,38 @@
+import io
+import json
+import os
+import cv2
+import pathlib
+import sys
+import torch
+import numpy as np
+import time
+import joblib
+from PIL import Image
+from flask import Flask, jsonify, request
+from torchvision import transforms
+
+
+
+app = Flask(__name__)
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+sys.path.insert(0, './yolov5')
+model = joblib.load('/Users/melod/graduate_/data.pkl')
+
+model.eval()      
+
+
+@app.route('/inference', methods=['POST'])
+def inference():
+    data = request.json
+    data = np.array(data['images'], dtype=np.uint8)
+    results = model(data)
+    model_out = results.pandas().xyxy[0]
+    
+    empty_seat_num = model_out['name'].value_counts()['empty']
+    return str(empty_seat_num)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=False)
